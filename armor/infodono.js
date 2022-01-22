ab24c4204ae20b4b5991fd7dd237f610e50d9ec1
qualquercoisa <qualquercoisa@gmail.com>
2022-01-22 14:47:04 -0300

    First commit

---
+++ armor/infodono.js
@@ -0,0 +1,14 @@
+const infodono = (prefix, numerodn) => {
+	return`
+
+╭┈┈「 ${NickDono} 」┈┈╮
+┊ 
+┊✾⋟ Proprietário: ⬇️
+┊✾⋟ wa.me/${numerodn} 
+┊✾⋟ Prefix : ${prefix} 
+┊
+╰┈┈❲ ${NomeDoBot} ❳┈┈╯
+`
+}
+
+exports.infodono = infodono
